4253c109512ed1a0f71b9df0ff36f6d05eafdd4f
Александр <1995BLADE@mail.ru>
2018-08-04 18:16:55 +0300

    Создание и подготовка проекта

---
+++ Zhovta Robot/View/MainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Zhovta_Robot
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
