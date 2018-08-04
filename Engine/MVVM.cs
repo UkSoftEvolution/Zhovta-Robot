d1d677bd3e5bbf3872bdc7947a420327882590d6
Александр <1995BLADE@mail.ru>
2018-08-04 18:31:42 +0300

    Создание движка и подключение к проекту

---
+++ Engine/MVVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Engine
+{
+    /// <summary>
+    /// Класс для работы с патерном MVVM
+    /// </summary>
+    public class MVVM : INotifyPropertyChanged
+    {
+        //INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+}
---
+++ Engine/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Engine")]
+[assembly: AssemblyDescription("Библиотека для побочных функций")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Uk. Soft-Evolution")]
+[assembly: AssemblyProduct("Engine")]
+[assembly: AssemblyCopyright("Copyright © Uk. Soft-Evolution 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d850b604-8540-4c1b-ab23-ccbddb5ac6df")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
